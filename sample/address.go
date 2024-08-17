6802f5b1fdfed1c1fa23272c61d9f09681195b11
tjmtmmnk <tjmtmmnk@gmail.com>
2024-08-17 17:56:11 +0900

    consider defined type of defined type

--- sample/address.go
+++ sample/address.go
@@ -2,6 +2,7 @@ package sample
 
 type (
 	AddressNumber  int
+	AddressNumber2 AddressNumber
 	AddressCity    string
 	AddressCountry string
 )
@@ -9,12 +10,17 @@ type (
 //go:generate go run github.com/tjmtmmnk/vogen -source $GOFILE -structs Address
 type Address struct {
 	Number  AddressNumber
+	Number2 AddressNumber2
 	City    AddressCity
 	Country AddressCountry
 }
 
 func NewAddressNumber(number int) (AddressNumber, error) {
 	return AddressNumber(number), nil
+}
+
+func NewAddressNumber2(number AddressNumber) (AddressNumber2, error) {
+	return AddressNumber2(number), nil
 }
 
 func NewAddressCity(city string) (AddressCity, error) {
